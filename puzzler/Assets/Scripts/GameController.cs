35b653781e40c2266d8c7bff10fec191bcd13f43
blacklion11 <mellodogg101@gmail.com>
2015-09-09 19:45:20 -0500

    basic block generation

---
+++ puzzler/Assets/Scripts/GameController.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GameController : MonoBehaviour {
+
+	public GameObject container;
+
+	public GameObject blue;
+	public GameObject black;
+	public GameObject brown;
+	public GameObject green;
+	public GameObject light_blue;
+	public GameObject red;
+
+	private ArrayList int_blocks;
+	private ArrayList blocks;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		// Construct the container bottom
+		for(int i = 0; i < 10; i++)
+		{
+			GameObject go = Instantiate(container) as GameObject;
+			go.transform.position = new Vector2(i, 0);
+		}
+
+		// Construct the container walls
+
+		for(int i = 0; i < 20; i++)
+		{
+			for(int j = 0; j < 10; j+=9)
+			{
+				GameObject go = Instantiate(container) as GameObject;
+				go.transform.position = new Vector2(j, i);
+
+			}
+
+		}
+
+		blocks = new ArrayList();
+		int_blocks = new ArrayList();
+
+		GenerateBlocks();
+		SmoothBlocks();
+		SpawnBlocks();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GenerateBlocks()
+	{
+		//Randomly generate the blocks
+		for(int i = 0; i < 19; i++)
+		{
+			int[] line = new int[8];
+			for(int j = 0; j < 8; j++)
+			{
+				int rand = (int) Random.Range(0f, 6f);
+				line[j] = rand;
+			}
+			int_blocks.Add(line);
+		}
+	}
+
+	void SpawnBlocks()
+	{
+		//Spawn the blocks 
+		for(int i = 1; i < 20; i++)
+		{
+			int[] int_line = (int[]) int_blocks[i - 1];
+			GameObject[] ob_line = new GameObject[8];
+			for(int j = 1; j < 9; j++)
+			{
+				int block_id = int_line[j - 1];
+				GameObject go = null;
+				switch(block_id)
+				{
+				case 0:
+					go = Instantiate(blue) as GameObject;
+					break;
+				case 1:
+					go = Instantiate(black) as GameObject;
+					break;
+				case 2:
+					go =Instantiate(brown) as GameObject;
+					break;
+				case 3:
+					go = Instantiate(green) as GameObject;
+					break;
+				case 4:
+					go = Instantiate(light_blue) as GameObject;
+					break;
+				case 5:
+					go = Instantiate(red) as GameObject;
+					break;
+				}
+
+				if(go != null)
+				{
+					// set block position
+					go.transform.position = new Vector2(j, i);
+
+					// add block to the line array
+					ob_line[j - 1] = go;
+				}
+			}
+			// add block line to arraylist
+			blocks.Add(ob_line);
+		}
+	}
+
+
+	void SmoothBlocks()
+	{
+
+		for(int i = 0; i < int_blocks.Count; i++)
+		{
+			int[] line = (int[]) int_blocks[i];
+			for(int j = 1; j < line.Length - 1; j++)
+			{
+				if(line[j] == line[j-1] && line[j] == line[j+1])
+				{
+					line[j] = (int) Random.Range(0f ,6f);
+					int_blocks[i]= line;
+				}
+
+			}
+
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
