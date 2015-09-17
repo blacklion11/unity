f54e5c001a6d776364fd271bf12567b8c87783c7
unknown <mellodogg101@gmail.com>
2015-09-17 12:20:37 -0500

    puzzler

---
+++ puzzler/Assets/Scripts/BlockController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour {
+
+
+	private Transform trans;
+
+	private Vector2 oldPos;
+
+	// Use this for initialization
+	void Start () {
+
+		trans = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	void OnMouseDown()
+	{
+
+		// Pick up block
+
+	}
+
+	void OnMouseDrag()
+	{
+
+		// Drag the block
+		Vector2 pos = new Vector2 (oldPos.x - Input.mousePosition.x, oldPos.y - Input.mousePosition.y);
+
+		trans.position = oldPos = pos;
+	}
+
+	void OnMouseUp()
+	{
+		// Check where the block was dropped
+	}
+}
