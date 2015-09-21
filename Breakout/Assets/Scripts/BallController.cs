df177ec273e97e2fe1b1578657393e0f341e18f9
unknown <mellodogg101@gmail.com>
2015-09-21 11:46:11 -0500

    Breakout

---
+++ Breakout/Assets/Scripts/BallController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	private Rigidbody2D ball;
+	public float speed;
+	private Vector2 lastVelocity;
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody2D> ();
+		float dirx = Random.Range (-3, 3);
+		float diry = Random.Range (1, 3);
+		ball.velocity = new Vector2 (speed * dirx, speed * diry);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		lastVelocity = ball.velocity;
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		ball.velocity = Vector2.Reflect(lastVelocity, collision.contacts[0].normal);
+
+	}
+}
---
+++ Breakout/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D paddle;
+	// Use this for initialization
+	void Start () {
+		paddle = GetComponent<Rigidbody2D>();
+		paddle.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		// move the player (paddle)
+		float dir = Input.GetAxis("Horizontal");
+		paddle.velocity = new Vector2 (speed * dir, 0.0f);
+	}
+}
