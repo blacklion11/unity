625f963b0f350e6620b61bd39196040b9d0e1584
unknown <mellodogg101@gmail.com>
2015-09-28 11:42:12 -0500

    crawler update

---
+++ Crawler/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public int health;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dirx = Input.GetAxis("Horizontal");
+        float diry = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(dirx * speed, diry * speed);
+        //this.transform.position = new Vector2(this.transform.position.x + dirx * speed, this.transform.position.y + diry * speed);
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collide");
+    }
+}
