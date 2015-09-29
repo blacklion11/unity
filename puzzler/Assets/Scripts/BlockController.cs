435480075cd10acfaa3f907467094f188a7a3e39
unknown <MELLOMJ-WKS@HOUSNG2663272.offices.uwec.edu>
2015-09-29 15:35:36 -0500

    puzzler

--- puzzler/Assets/Scripts/BlockController.cs
+++ puzzler/Assets/Scripts/BlockController.cs
@@ -2,15 +2,18 @@
 using System.Collections;
 
 public class BlockController : MonoBehaviour {
-	//public static GameController S;
 
 	private Transform trans;
 	private SpriteRenderer renderer;
 	private Color selectColor;
+	private Color deselectColor;
 	private bool isSelected;
 
 	private Vector2 oldPos;
 	private Vector2 mousePos;
+	private int id;
+	private int color_id;
+	public bool counted;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +26,13 @@ public class BlockController : MonoBehaviour {
 		selectColor.g = 0.25f;
 		selectColor.b = 0.25f;
 		selectColor.a = 1f;
+
+		deselectColor = new Color();
+		deselectColor.r = 1f;
+		deselectColor.g = 1f;
+		deselectColor.b = 1f;
+		deselectColor.a = 1f;
+
 	}
 	
 	// Update is called once per frame
@@ -46,7 +56,11 @@ public class BlockController : MonoBehaviour {
 	void OnMouseUp()
 	{
 		if (!isSelected) {
-			StartCoroutine(GameController.S.SelectBlock(this));
+			StartCoroutine(GameController.S.SelectBlock(this, true));
+		}
+		else
+		{
+			StartCoroutine(GameController.S.SelectBlock(this,false));
 		}
 	}
 
@@ -58,10 +72,23 @@ public class BlockController : MonoBehaviour {
 		}
 		else
 		{
-			renderer.color = Color.white;
+			renderer.color = deselectColor;
 		}
 
 		yield return new WaitForSeconds(0);
+	}
+
+	public int ID
+	{ 
+		get { return id;}
+		set { id = value;}
+	}	
+
+	public int Color_ID
+	{
+		get { return color_id; }
+		set { color_id = value; }
+
 	}
 }
 
--- puzzler/Assets/Scripts/GameController.cs
+++ puzzler/Assets/Scripts/GameController.cs
@@ -58,22 +58,48 @@ public class GameController : MonoBehaviour {
 	private BlockController blockA;
 	private BlockController blockB;
 
-	public IEnumerator SelectBlock(BlockController block)
+	public IEnumerator SelectBlock(BlockController block, bool value)
 	{
-		if(blockA == null)
+		if(!value)
 		{
-			blockA = block;
-
-			yield return StartCoroutine(block.Select(true));
+			blockA = null;
+			yield return StartCoroutine(block.Select(false));
 		}
-		else if(blockB == null && isNeighbor(blockA.transform, block.transform))
+		else
 		{
-			blockB = block;
+			if(blockA == null)
+			{
+				blockA = block;
+
+				yield return StartCoroutine(block.Select(true));
+			}
+			else if(blockB == null && isNeighbor(blockA.transform, block.transform))
+			{
+				blockB = block;
 
 
-			yield return StartCoroutine(block.Select(true));
+				yield return StartCoroutine(block.Select(true));
+
+				yield return new WaitForSeconds(0.5f);
+				
+				Vector2 tempPos = blockA.transform.position;
+				
+				blockA.transform.position = blockB.transform.position;
+				blockB.transform.position = tempPos;
+
+				yield return StartCoroutine(blockA.Select(false));
+				yield return StartCoroutine(blockB.Select(false));
+
+				// Check for groups to remove
+				removeGroup();
+
+
+				// shift blocks after removing groups
+				shiftBlocks();
+
+				blockA = blockB = null;
+			}
 		}
-
 		yield return new WaitForSeconds (0);
 
 	}
@@ -81,8 +107,67 @@ public class GameController : MonoBehaviour {
 	bool isNeighbor(Transform transA, Transform transB)
 	{
 		return Mathf.Abs(transA.position.x - transB.position.x) <= 1 && Mathf.Abs(transA.position.y - transB.position.y) <= 1; 
+	}
+
+
+	void shiftBlocks()
+	{
 
 	}
+
+
+	void removeGroup()
+	{
+
+		RaycastHit2D rayhit = Physics2D.Raycast(blockA.transform.position, Vector2.up);
+
+		Debug.Log(rayhit.distance);
+		if(rayhit.collider.gameObject.GetComponent<BlockController>().Color_ID == blockA.Color_ID) Destroy(rayhit.collider.gameObject);
+	}
+
+
+	/*
+	bool walkBlocks(BlockController block, int counter)
+	{
+		int numLikeBlocks = 0;
+		bool shouldDestroy = false;
+		// walk through all touching blocks and see if they are the same color
+		for(int i = -1; i < 2; i++)
+		{
+			for(int j = -1; j < 2; j++)
+			{
+				if(i == 0 && j == 0)continue;
+
+				Debug.Log(block.transform.position);
+
+				GameObject testBlock =  ( (GameObject[]) blocks[(int) (block.transform.position.y) + i])[((int) block.transform.position.x) + j];
+				if(!testBlock.GetComponent<BlockController>().counted && testBlock.GetComponent<BlockController>().Color_ID == block.Color_ID)
+				{
+					// match found
+					testBlock.GetComponent<BlockController>().counted = true;
+					numLikeBlocks++;
+
+					// test the test block for like colored blocks
+					shouldDestroy = walkBlocks(testBlock.GetComponent<BlockController>(), counter + numLikeBlocks);
+				}
+			}
+		}
+
+		// if no blocks are the same color check counter for number of touching same-colored blocks
+		if(counter + numLikeBlocks > 3)
+		{
+			shouldDestroy = true;
+		}
+
+		if(shouldDestroy)
+		{
+			Destroy(block.gameObject);
+		}
+		return shouldDestroy;
+	}
+	*/
+
+
 
 	void GenerateBlocks()
 	{
@@ -92,7 +177,7 @@ public class GameController : MonoBehaviour {
 			int[] line = new int[8];
 			for(int j = 0; j < 8; j++)
 			{
-				int rand = (int) Random.Range(0f, 4f);
+				int rand = (int) Random.Range(0f, 80);
 				line[j] = rand;
 			}
 			int_blocks.Add(line);
@@ -110,27 +195,41 @@ public class GameController : MonoBehaviour {
 			{
 				int block_id = int_line[j - 1];
 				GameObject go = null;
-				switch(block_id)
+				if(block_id < 80)
 				{
-				case 0:
 					go = Instantiate(blue) as GameObject;
-					break;
-				case 1:
-					go =Instantiate(brown) as GameObject;
-					break;
-				case 2:
-					go = Instantiate(green) as GameObject;
-					break;
-				case 3:
-					go = Instantiate(light_blue) as GameObject;
-					break;
-				case 4:
-					go = Instantiate(red) as GameObject;
-					break;
+					go.GetComponent<BlockController>().Color_ID = 0;
 				}
+				if(block_id < 60)
+				{
+					if(go != null) Destroy(go.gameObject);
+					go =Instantiate(brown) as GameObject;
+					go.GetComponent<BlockController>().Color_ID = 1;
+				}
+				if(block_id < 40)
+				{
+					if(go != null) Destroy(go.gameObject);
+					go = Instantiate(green) as GameObject;
+					go.GetComponent<BlockController>().Color_ID = 2;
+				}
+				if(block_id < 20)
+				{
+					if(go != null) Destroy(go.gameObject);
+					go = Instantiate(light_blue) as GameObject;
+					go.GetComponent<BlockController>().Color_ID = 3;
+				}
+				if (block_id < 10)
+				{
+					if(go != null) Destroy(go.gameObject);
+					go = Instantiate(red) as GameObject;
+					go.GetComponent<BlockController>().Color_ID = 4;
+				}
+		
 
 				if(go != null)
 				{
+					//make sure the blocks dont rotate
+
 					// set block position
 					go.transform.position = new Vector2(j, i);
 
