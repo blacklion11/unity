53d375f0e1b7b87db6c7f079485ba903deb4ce06
unknown <mellodogg101@gmail.com>
2015-10-12 11:29:38 -0500

    crawler update

---
+++ Crawler/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+
+    private GameObject player;
+    public float zoom;
+    private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        this.player = GameObject.FindWithTag("Player");
+        camera = GetComponent<Camera>();
+	}
+
+    public void assignPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (player != null)
+        {
+            this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+            camera.orthographicSize = zoom;
+        }
+	}
+}
---
+++ slerp/Assets/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject circle;
+    private float radius;
+
+    private Vector3 world_pos;
+	// Use this for initialization
+	void Start () {
+        radius = circle.transform.localScale.x / 2;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 screen_pos = Input.mousePosition;
+            world_pos = Camera.main.ScreenToWorldPoint(screen_pos);
+            world_pos.z = 0;
+            Debug.Log(world_pos);
+        }
+	}
+
+    IEnumerator moveto()
+    {
+        Vector3 source = transform.position;
+
+        Vector3 destination = GetComponent<SphereCollider>().ClosestPointOnBounds(world_pos);
+        Debug.Log(destination);
+        return null;
+    }
+    
+}
