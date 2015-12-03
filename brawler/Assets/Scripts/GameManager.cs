bc8929b56123b63ad750ccf071a10ec50fc96f9a
unknown <MELLOMJ-WKS@HOUSNG2663272.offices.uwec.edu>
2015-12-03 17:34:02 -0600

    crawler update

---
+++ brawler/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+	public ArrayList<GameObject> players;
+
+	void Start ()
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+}
